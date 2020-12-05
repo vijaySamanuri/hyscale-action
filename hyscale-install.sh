99bec5e2041e2c73e7831d6b0ad550b818a36775
vijaysamanuri <vijay.samanuri@hyscale.io>
2020-12-06 01:12:42 +0530

    Fixed typo

--- hyscale-install.sh
+++ hyscale-install.sh
@@ -1,7 +1,7 @@
 hyscale_version="$HYSCALE_VERSION"
 docker pull hyscale/hyscale:${hyscale_version}
 
-temp_container_name="hyscale=$(date +%s)"
+temp_container_name="hyscale-$(date +%s)"
 docker create --name ${temp_container_name} hyscale/hyscale:${hyscale_version} 
 
 docker cp $temp_container_name:/usr/local/bin/hyscale.jar /usr/local/bin/hyscale.jar
